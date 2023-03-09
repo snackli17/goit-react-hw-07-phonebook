af848f8ad1c1baa76c89393cb5565f2e4d2022e0
LifeLong-LeArneR17 <snackli17@gmail.com>
2023-03-09 17:40:55 +0200

    fix
    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { ContactList } from './ContactList/ContactList';
 export const App = () => {
   return (
     <>
-      <div style={{ marginLeft: '40px' }}>
+      <div style={{ marginLeft: '30px' }}>
         <h1>Phonebook</h1>
         <ContactForm />
         <h2>Contacts</h2>
